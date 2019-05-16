70b02c703d3eae017d0d12d59029b798c70af758
Ridhomrpp <Ridhom887@gmail.com>
2019-05-16 23:38:20 +0700

    komit pertama

---
+++ pendaftaran_anggota.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 16, 2019 at 06:06 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+create database pendaftaran_siswa;
+use pendaftaran_siswa;
+--
+-- Database: `pendaftaran_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `calon_siswa`
+--
+
+CREATE TABLE `calon_siswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(64) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(16) NOT NULL,
+  `agama` varchar(16) NOT NULL,
+  `sekolah_asal` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `calon_siswa`
+--
+
+INSERT INTO `calon_siswa` (`id`, `nama`, `alamat`, `jenis_kelamin`, `agama`, `sekolah_asal`) VALUES
+(165610079, 'Mohammad Quirni', 'Jl. Janti', 'Laki - Laki', 'Islam', 'SMK Negeri 6 Palu'),
+(165610082, 'Deny Tofan', 'Jl. Janti', 'laki-laki', 'Islam', 'SMK Negeri 3 Balikpapan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `calon_siswa`
+--
+ALTER TABLE `calon_siswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=165610083;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
